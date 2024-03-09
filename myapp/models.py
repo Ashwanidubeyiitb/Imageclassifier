5f9d4e34f0d452b8047260dac640f11778f345c0
Ashwanidubeyiitb <dubeyash2004@gmail.com>
2024-03-09 14:51:51 +0530

    1st

--- myapp/models.py
+++ myapp/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Image(models.Model):
  photo = models.ImageField(upload_to="myimage")
  date = models.DateTimeField(auto_now_add=True)
+ classification = models.CharField(max_length=10, blank=True)
